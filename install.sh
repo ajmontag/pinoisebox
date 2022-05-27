09ce03ce45a890d1b8da4e37d7032b0bcf41cfac
Andrew Montag <ajmontag@gmail.com>
2022-05-27 18:13:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ mkdir -p $PINOISEBOX_HOME_DIR/
 mkdir -p $PINOISEBOX_HOME_DIR/media
 mkdir -p $PINOISEBOX_HOME_DIR/src
 cp -r ./ $PINOISEBOX_HOME_DIR/src
-cd $PINOISEBOX_HOME_DIR
+cd $PINOISEBOX_HOME_DIR/media
 
 # download media
 # https://mc2method.org/white-noise/
 
 # fan 6
-wget --no-check-certificate --directory-prefix=media -O fan6.mp3 https://mc2method.org/white-noise/download.php?file=33-Fan&length=60
-ln -s -f media/fan6.mp3 media/default.mp3
+wget --no-check-certificate -O fan6.mp3 https://mc2method.org/white-noise/download.php?file=33-Fan&length=60
+ln -s -f fan6.mp3 default.mp3
 
 install src/*.service /etc/systemd/system/
 
