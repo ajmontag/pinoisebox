bb5ce9106908fff911442d8ea6e3b65e965c848d
Andrew Montag <ajmontag@gmail.com>
2022-05-27 20:47:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ if [[ $EUID -ne 0 ]]; then
    exit 100
 fi
 
-# apt-get update
-# apt-get install -y mpg123
-
 export PINOISEBOX_HOME_DIR=/opt/pinoisebox
 
 mkdir -p $PINOISEBOX_HOME_DIR
@@ -17,8 +14,7 @@ cd $PINOISEBOX_HOME_DIR
 # media from:
 # https://mc2method.org/white-noise/
 
-mpg123 -w fan6.wav fan6.mp3
-ln -s -f fan6.wav default.wav
+ln -s -f fan6.mp3 default.mp3
 
 install src/*.service /etc/systemd/system/
 
